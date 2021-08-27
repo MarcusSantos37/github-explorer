92392094d4882e9cc7271a777691a6ef17c75f04
Marcus Augusto <marcusastrade@gmail.com>
2021-08-27 19:09:37 -0300

    backup

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,22 +2,24 @@ import { useEffect, useState } from "react";
 
 import { RepositoryItem } from "./RepositoryItem";
 
-import "../styles/repositories.scss";
+import "../styles/issues.scss";
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [issues, setIssues] = useState([]);
+
+  console.log("issues", issues);
 
   useEffect(() => {
-    fetch("https://api.github.com/orgs/rocketseat/repos")
+    fetch("https://github.com/facebook/react/issues")
       .then((response) => response.json())
-      .then((data) => setRepositories(data));
+      .then((data) => setIssues(data));
   }, []);
 
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        {repositories.map((repository) => {
+        {issues.map((repository) => {
           return (
             <RepositoryItem key={repository.name} repository={repository} />
           );
